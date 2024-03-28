afb3d700362b6a3d57c1c3b935c401f5ba4c733a
rbbozkurt <resit.b.bozkuXrt@campus.tu-berlin.de>
2024-03-28 23:09:31 +0100

    WaterCounter and WellnessScreen  implemented.

---
+++ app/src/main/java/com/rbbozkurt/basicstatecodelab/ui/theme/view/WellnessScreen.kt
@@ -0,0 +1,9 @@
+package com.rbbozkurt.basicstatecodelab.ui.theme.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessScreen(modifier : Modifier = Modifier) {
+    WaterCounter()
+}
